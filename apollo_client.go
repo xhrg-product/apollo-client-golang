f196e54c3726aad66023ad10071c19c0db0e3b08
xhrg <634789257@qq.com>
2020-09-13 17:43:01 +0800

    完成第一个版本
    完成第一个版本

--- apollo_client.go
+++ apollo_client.go
@@ -5,7 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/url"
 	"sync"
+	"time"
 )
 
 func NewApolloClient(configServerUrl string, appId string, cluster string) *apolloClint {
@@ -29,15 +32,16 @@ type apolloClint struct {
 	cache     sync.Map
 	dataHash  sync.Map
 	cycleTime int
+	stop      bool
 }
 
-func (client apolloClint) GetValue(key string, namespace string, defaultVal string) string {
+func (client *apolloClint) GetValue(key string, namespace string, defaultVal string) string {
 	if namespace == "" {
 		namespace = "application"
 	}
 	//读取本地缓存
-	if namespaceCache, ok := client.cache.Load(key); ok {
-		kvData := namespaceCache.(namespaceData).Configurations
+	if namespaceCache, ok := client.cache.Load(namespace); ok {
+		kvData := namespaceCache.(*namespaceData).Configurations
 		if val, ok := kvData[key]; ok {
 			return val.(string)
 		}
@@ -68,7 +72,7 @@ func (client apolloClint) GetValue(key string, namespace string, defaultVal stri
 	return defaultVal
 }
 
-func (client apolloClint) getFileCache(namespace string) *namespaceData {
+func (client *apolloClint) getFileCache(namespace string) *namespaceData {
 	filePath := client.filePath(namespace)
 	bytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -83,14 +87,18 @@ func (client apolloClint) getFileCache(namespace string) *namespaceData {
 }
 
 //更新本地缓存
-func (client apolloClint) updateCache(namespace string, data *namespaceData) {
+func (client *apolloClint) updateCache(namespace string, data *namespaceData) {
 	if namespace == "" || data == nil {
+		return
+	}
+	if data.NotificationId != EmptyNotificationId {
+		client.cache.Store(namespace, data)
 		return
 	}
 	nid := data.NotificationId
 	if d, ok := client.cache.Load(namespace); ok {
-		nd := d.(namespaceData)
-		if nd.NotificationId != "" {
+		nd := d.(*namespaceData)
+		if nd.NotificationId > 0 {
 			nid = nd.NotificationId
 		}
 	}
@@ -98,7 +106,7 @@ func (client apolloClint) updateCache(namespace string, data *namespaceData) {
 	client.cache.Store(namespace, data)
 }
 
-func (client apolloClint) updateFile(namespace string, data *namespaceData) {
+func (client *apolloClint) updateFile(namespace string, data *namespaceData) {
 
 	bytes, error := json.Marshal(data)
 	if error != nil {
@@ -115,16 +123,16 @@ func (client apolloClint) updateFile(namespace string, data *namespaceData) {
 	ioutil.WriteFile(filePath, bytes, 0666)
 }
 
-func (client apolloClint) filePath(namespace string) string {
+func (client *apolloClint) filePath(namespace string) string {
 	return fmt.Sprintf("%s%s_configuration_%s.txt", client.CacheFilePath, client.AppId, namespace)
 }
 
-func (client apolloClint) getFromNet(namespace string) *namespaceData {
+func (client *apolloClint) getFromNet(namespace string) *namespaceData {
 
 	url := fmt.Sprintf("%s/configfiles/json/%s/%s/%s?ip=%s", client.ConfigServerUrl, client.AppId, client.Cluster, namespace, client.Ip)
 	_, body := httpRequest(url, 3)
 
-	namespaceData := &namespaceData{NotificationId: "0"}
+	namespaceData := &namespaceData{NotificationId: EmptyNotificationId}
 	mapKv := make(map[string]interface{})
 
 	error := json.Unmarshal([]byte(body), &mapKv)
@@ -133,4 +141,72 @@ func (client apolloClint) getFromNet(namespace string) *namespaceData {
 	}
 	namespaceData.Configurations = mapKv
 	return namespaceData
+}
+
+func (client *apolloClint) startHotUpdate() {
+	go func() {
+		for true {
+			time.Sleep(time.Second * 3)
+			if client.stop {
+				return
+			}
+			client.hotUpdate()
+		}
+	}()
+}
+
+func (client *apolloClint) hotUpdate() {
+
+	var array []*notificationDto
+
+	client.cache.Range(func(key, value interface{}) bool {
+		if value != nil {
+			array = append(array, &notificationDto{NamespaceName: key.(string), NotificationId: value.(*namespaceData).NotificationId})
+		}
+		return true
+	})
+
+	if len(array) == 0 {
+		return
+	}
+
+	bytes, err := json.Marshal(array)
+	if err != nil {
+		return
+	}
+	params := url.Values{}
+	params.Add("appId", client.AppId)
+	params.Add("cluster", client.Cluster)
+	params.Add("notifications", string(bytes))
+	paramStr := params.Encode()
+	url := fmt.Sprintf("%s/notifications/v2?%s", client.ConfigServerUrl, paramStr)
+	code, body := httpRequest(url, 75)
+	if code == 304 {
+		return
+	}
+	if code == errorCode {
+		return
+	}
+	if code == 200 {
+		var array1 []*notificationDto
+		err := json.Unmarshal([]byte(body), &array1)
+		if err != nil {
+			log.Println(err)
+		}
+		//[{"namespaceName":"application","notificationId":974,"messages":{"details":{"demo-service+default+application":974}}}]
+		if len(array1) == 0 {
+			return
+		}
+		for _, e := range array1 {
+			namespaceName := e.NamespaceName
+			data := client.getFromNet(namespaceName)
+			if data == nil {
+				continue
+			}
+			data.NotificationId = e.NotificationId
+			client.updateCache(namespaceName, data)
+			client.updateFile(namespaceName, data)
+		}
+	}
+
 }
--- apollo_client_test.go
+++ apollo_client_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestIp(t *testing.T) {
@@ -22,5 +23,13 @@ func TestNewApolloClient(t *testing.T) {
 	url := os.Getenv("APOLLO_CONFIG_URL")
 	log.Println(url)
 	client := NewApolloClient(url, "demo-service", "DEV")
-	client.GetValue("name", "", "")
+	client.GetValue("name", "application", "")
+	client.startHotUpdate()
+
+	for true {
+		log.Println(client.GetValue("name", "application", "aa"))
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Hour * 1)
 }
